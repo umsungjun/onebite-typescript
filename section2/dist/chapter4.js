fc413f3a6457d8c695b2aab78fb4477582f3caa1
umsungjun <umseongjun@naver.com>
2025-07-22 21:42:57 +0900

    Update chapter4.js

--- section2/dist/chapter4.js
+++ section2/dist/chapter4.js
@@ -1,17 +1,15 @@
-let user = {
-    id: 1,
-    name: "이정환",
-    nickname: "winterlood",
-    birth: "1997.01.07",
-    bio: "안녕하세요",
-    location: "부천시",
+let dog = {
+    age: 3,
+    name: "멍멍이",
 };
-let user2 = {
-    id: 2,
-    name: "홍길동",
-    nickname: "winterlood",
-    birth: "1997.01.07",
-    bio: "안녕하세요",
-    location: "부천시",
+let countryCodes = {
+    Korea: "ko",
+    UnitedState: "us",
+    UnitedKingdom: "uk",
+};
+let countryNumberAndStringCodes = {
+    Korea: 1,
+    UnitedState: 2,
+    UnitedKingdom: 3,
 };
 export {};
